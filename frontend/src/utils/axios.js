86ae7177b40c1eecf4b1bcf441af6ae08353eada
sadhana <sadhana903545@gmail.com>
2025-11-27 16:58:42 +0530

    import

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -4,7 +4,7 @@ import qs from "qs";
 // Serializtion -> converting object or array into an url query String that can be sent in an HTTP request.
 
 export const axiosInstance = axios.create({
-  baseURL: process.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
 });
